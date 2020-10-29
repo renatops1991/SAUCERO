91d7b62d0cdab8cad595c0b4be7ba9c65a0aae9b
Renato Pereira <renatops1991@gmail.com>
2020-10-29 16:23:24 -0300

    Inicialização da marcação do HTML

---
+++ index.php
@@ -0,0 +1,31 @@
+<!doctype html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Saucero</title>
+</head>
+<body>
+    <header>
+        <div>
+            <img src="" alt="">
+            <div>
+                <span>(11)9 4672-0947</span>
+                <span>9:00 - 17:00</span>
+            </div>
+        </div>
+        <div>
+            <img src="" alt="Saucero">
+        </div>
+
+        <div>
+            <div>
+                <span>R$ 0,00</span>
+                <span>0 itens</span>
+            </div>
+        </div>
+    </header>
+</body>
+</html>
