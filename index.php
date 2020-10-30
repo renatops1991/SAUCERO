a700ba28d82705b9e352eafcb63047a17ed9adef
Renato Pereira <renatops1991@gmail.com>
2020-10-30 09:36:47 -0300

    Marcação do HTML - ok

--- index.php
+++ index.php
@@ -8,24 +8,255 @@
     <title>Saucero</title>
 </head>
 <body>
-    <header>
+<header>
+    <div>
+        <img src="" alt="">
         <div>
-            <img src="" alt="">
-            <div>
-                <span>(11)9 4672-0947</span>
-                <span>9:00 - 17:00</span>
-            </div>
+            <span>(11)9 4672-0947</span>
+            <span>9:00 - 17:00</span>
         </div>
+    </div>
+    <div>
+        <img src="" alt="Saucero">
+
+        <nav>
+            <ul>
+                <li><a href="">Home</a></li>
+                <li><a href="">Menu</a></li>
+                <li><a href="">Features</a></li>
+                <li><a href="">About</a></li>
+                <li><a href="">Contact</a></li>
+            </ul>
+        </nav>
+    </div>
+
+    <div>
         <div>
-            <img src="" alt="Saucero">
+            <span>R$ 0,00</span>
+            <span>0 itens</span>
+        </div>
+    </div>
+</header>
+
+<main>
+    <section>
+        <div>
+            <article>
+                <h2>Vincent Pizza</h2>
+                <h3>making people happy</h3>
+
+                <footer>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Facilis, repellat!</p>
+                    <a href="">Book Now</a> <a href="">View Menu</a>
+                </footer>
+            </article>
+            <div>
+                <img src="" alt="Imagem do banner">
+            </div>
         </div>
 
         <div>
+            <article>
+                <h2>Vincent Pizza 2</h2>
+                <h3>making people happy 2</h3>
+
+                <footer>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Facilis, repellat!</p>
+                    <a href="">Book Now</a> <a href="">View Menu</a>
+                </footer>
+            </article>
             <div>
-                <span>R$ 0,00</span>
-                <span>0 itens</span>
+                <img src="" alt="Imagem do banner">
             </div>
         </div>
-    </header>
+
+        <div>
+            <div><a href="" title="previous">Anterior</a></div>
+            <div><a href="" title="next">Próximo</a></div>
+        </div>
+    </section>
+
+    <section>
+        <div>
+            <article>
+                <img src="" alt="Quality Foods">
+                <h2>Quality Foods</h2>
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ipsam, nisi?</p>
+            </article>
+            <article>
+                <img src="" alt="Quality Foods">
+                <h2>Quality Foods</h2>
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ipsam, nisi?</p>
+            </article>
+            <article>
+                <img src="" alt="Quality Foods">
+                <h2>Quality Foods</h2>
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ipsam, nisi?</p>
+            </article>
+        </div>
+    </section>
+
+    <section>
+        <header>
+            <h1>Hot Sales</h1>
+        </header>
+        <div>
+            <article>
+                <img src="" alt="">
+                <h2>Diablo</h2>
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Maiores, nobis.</p>
+                <p><span>R$ 20,00</span></p>
+            </article>
+
+            <article>
+                <img src="" alt="">
+                <h2>Carbonara</h2>
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Maiores, nobis.</p>
+                <p><span>R$ 20,00</span></p>
+            </article>
+
+            <article>
+                <img src="" alt="">
+                <h2>Capricciosa</h2>
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Maiores, nobis.</p>
+                <p><span>R$ 20,00</span></p>
+            </article>
+
+            <article>
+                <img src="" alt="">
+                <h2>Prosciutto</h2>
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Maiores, nobis.</p>
+                <p><span>R$ 20,00</span></p>
+            </article>
+        </div>
+    </section>
+
+    <section>
+        <header>
+            <h1>Discover our menu</h1>
+        </header>
+        <div>
+            <ul>
+                <li><a href="">Burgers</a></li>
+                <li><a href="">Desserts</a></li>
+                <li><a href="">Drinks</a></li>
+                <li><a href="">Pasta</a></li>
+                <li><a href="">Pizzas</a></li>
+                <li><a href="">Salads</a></li>
+            </ul>
+
+            <article>
+                <img src="" alt="">
+                <h2>Lorem ipsum dolor.</h2>
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusamus, iste.</p>
+                <p><span>R$ 20,00</span></p>
+            </article>
+
+            <article>
+                <img src="" alt="">
+                <h2>Lorem ipsum dolor.</h2>
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusamus, iste.</p>
+                <p><span>R$ 20,00</span></p>
+            </article>
+
+            <article>
+                <img src="" alt="">
+                <h2>Lorem ipsum dolor.</h2>
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusamus, iste.</p>
+                <p><span>R$ 20,00</span></p>
+            </article>
+
+            <article>
+                <img src="" alt="">
+                <h2>Lorem ipsum dolor.</h2>
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusamus, iste.</p>
+                <p><span>R$ 20,00</span></p>
+            </article>
+
+            <article>
+                <img src="" alt="">
+                <h2>Lorem ipsum dolor.</h2>
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusamus, iste.</p>
+                <p><span>R$ 20,00</span></p>
+            </article>
+
+            <article>
+                <img src="" alt="">
+                <h2>Lorem ipsum dolor.</h2>
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusamus, iste.</p>
+                <p><span>R$ 20,00</span></p>
+            </article>
+
+            <article>
+                <img src="" alt="">
+                <h2>Lorem ipsum dolor.</h2>
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusamus, iste.</p>
+                <p><span>R$ 20,00</span></p>
+            </article>
+
+            <article>
+                <img src="" alt="">
+                <h2>Lorem ipsum dolor.</h2>
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusamus, iste.</p>
+                <p><span>R$ 20,00</span></p>
+            </article>
+        </div>
+    </section>
+
+    <section>
+        <header>
+            <h1>Contact us</h1>
+        </header>
+
+        <div>
+            <form action="" method="post" enctype="multipart/form-data">
+                <div>
+                    <input type="text" name="name" placeholder="Seu nome">
+                </div>
+
+                <div>
+                    <input type="email" name="email" placeholder="Seu e-mail">
+                </div>
+
+                <div>
+                    <input type="text" name="phone" placeholder="Seu telefone">
+                </div>
+
+                <div>
+                    <textarea name="message" cols="30" rows="10"></textarea>
+                </div>
+
+                <button type="submit">Enviar mensagem</button>
+            </form>
+        </div>
+    </section>
+</main>
+
+<div>
+    <a href="" title="Subir para o topo">Subir</a>
+</div>
+
+<footer>
+    <div>
+        <img src="" alt="Saucero">
+        <p>(11)9 4672-0947 - 09:00 - 17:00</p>
+
+        <ul>
+            <li><a href="">Home</a></li>
+            <li><a href="">Menu</a></li>
+            <li><a href="">Features</a></li>
+            <li><a href="">About</a></li>
+            <li><a href="">Contact</a></li>
+        </ul>
+
+        <ul>
+            <li><a href="">Twiter</a></li>
+            <li><a href="">Facebook</a></li>
+            <li><a href="">Instagram</a></li>
+        </ul>
+
+        <p>&copy; 2020 - Todos os direitos reservados</p>
+    </div>
+</footer>
 </body>
 </html>
